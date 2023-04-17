f9eef2661dc55e6c808f6816830cc11539dbdf09
data-miner00 <data-miner00@users.noreply.github.com>
2023-04-17 22:01:44 +0800

    feat: Added deployment network

--- apps/contracts/hardhat.config.ts
+++ apps/contracts/hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
+import { mnemonic } from "./settings";
+
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+
+  networks: {
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      accounts: {
+        mnemonic,
+      },
+    },
+  },
 };
 
 export default config;
---
+++ apps/contracts/settings.ts
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const mnemonic = process.env.ACCOUNT_MNEMONIC;
